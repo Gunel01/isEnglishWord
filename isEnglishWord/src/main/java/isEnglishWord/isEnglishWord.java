5823721a8dbd5bd961b573d39e6dc7540d2b2173
Gunel01 <user@192.168.2.240>
2021-05-03 12:17:11 -0400

    n

---
+++ isEnglishWord/src/main/java/isEnglishWord/isEnglishWord.java
@@ -0,0 +1,11 @@
+package isEnglishWord;
+
+public class isEnglishWord  {
+
+	public static void main(String[] args) throws Exception  {
+
+
+        System.out.println(Dictionary.isEnglishWord( "made" ));
+	}
+
+}
